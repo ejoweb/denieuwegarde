5691652b4fb30ab54d2874d2e50a3992022a0fd8
Erik Joling <info@ejoweb.nl>
2023-04-12 12:04:33 +0200

    Improve templates and patterns

--- patterns/footer-default.php
+++ patterns/footer-default.php
@@ -6,46 +6,8 @@
  * Block Types: core/template-part/footer
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:columns {"align":"wide"} -->
-<div class="wp-block-columns alignwide"><!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading {"fontSize":"large"} -->
-<h2 class="wp-block-heading has-large-font-size">Wil je meer weten?</h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph -->
-<p>Kom in contact met ons:<br><a href="mailto:info@denieuwegarde.nl">info@denieuwegarde.nl</a></p>
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-base-color has-contrast-background-color has-text-color has-background has-link-color"><!-- wp:paragraph {"align":"center","fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size">© De Nieuwe Garde Nieuwleusen</p>
 <!-- /wp:paragraph --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"50%"} -->
-<div class="wp-block-column" style="flex-basis:50%"><!-- wp:heading {"fontSize":"large"} -->
-<h2 class="wp-block-heading has-large-font-size">De Nieuwe Garde</h2>
-<!-- /wp:heading -->
-
-<!-- wp:list -->
-<ul><!-- wp:list-item -->
-<li>Een link of tekst</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>Een link of tekst</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>Een link of tekst</li>
-<!-- /wp:list-item -->
-
-<!-- wp:list-item -->
-<li>Een link of tekst</li>
-<!-- /wp:list-item --></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
-
-<!-- wp:group {"align":"wide","style":{"border":{"top":{"color":"var:preset|color|background","width":"1px"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group alignwide" style="border-top-color:var(--wp--preset--color--background);border-top-width:1px"><!-- wp:paragraph {"fontSize":"small"} -->
-<p class="has-small-font-size">© De Nieuwe Garde</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
 <!-- /wp:group -->
--- patterns/header-default.php
+++ patterns/header-default.php
@@ -6,12 +6,18 @@
  * Block Types: core/template-part/header
  */
 ?>
-<!-- wp:group {"layout":{"type":"constrained","justifyContent":"center","contentSize":""}} -->
-<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide"><!-- wp:site-title {"fontSize":"large"} /-->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"backgroundColor":"contrast","layout":{"type":"default"}} -->
+<div class="wp-block-group has-contrast-background-color has-background" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|50","bottom":"var:preset|spacing|40","left":"var:preset|spacing|50"}}},"textColor":"base","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-base-color has-text-color has-link-color" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph -->
+<p>Nieuwleusen - <a rel="noreferrer noopener" href="https://www.despilnieuwleusen.nl/" target="_blank">Kulturhus De Spil</a> </p>
+<!-- /wp:paragraph -->
 
-<!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"secondary","overlayTextColor":"background"} -->
-<!-- wp:page-list /-->
-<!-- /wp:navigation --></div>
+<!-- wp:paragraph -->
+<p><strong><a href="/#contact-openingstijden" data-type="URL" data-id="/#contact-openingstijden">Contact & Openingstijden</a></strong></p>
+<!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
+
+<!-- wp:cover {"overlayColor":"base","minHeight":50,"isDark":false,"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-cover is-light" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-base-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:site-logo {"width":700,"isLink":false,"shouldSyncIcon":true,"align":"center","style":{"spacing":{"margin":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","bottom":"var:preset|spacing|40","top":"var:preset|spacing|30"}}}} /--></div></div>
+<!-- /wp:cover -->
